bbb2750cfe565a3a06f783fa31254a4a2879111d
Jon2G <jonyjouna@gmail.com>
2022-05-16 01:57:31 -0500

    Agregar lecturas y conectar con proceso para el envio de muestras

--- RF_Control/ArrowsControl.xaml.cs
+++ RF_Control/ArrowsControl.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RF_Control
 {
@@ -23,6 +11,26 @@ namespace RF_Control
         public ArrowsControl()
         {
             InitializeComponent();
+        }
+
+        private void Up_Click(object sender, RoutedEventArgs e)
+        {
+            RFController.Move(RFController.DIRECTIONS.UP);
+        }
+
+        private void left_Click(object sender, RoutedEventArgs e)
+        {
+            RFController.Move(RFController.DIRECTIONS.LEFT);
+        }
+
+        private void right_Click(object sender, RoutedEventArgs e)
+        {
+            RFController.Move(RFController.DIRECTIONS.RIGHT);
+        }
+
+        private void down_Click(object sender, RoutedEventArgs e)
+        {
+            RFController.Move(RFController.DIRECTIONS.DOWN);
         }
     }
 }
---
+++ RF_Control/RFController.cs
@@ -0,0 +1,57 @@
+﻿using AsyncAwaitBestPractices;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace RF_Control
+{
+    public static class RFController
+    {
+        public enum DIRECTIONS
+        {
+            UP, DOWN, RIGHT, LEFT
+        }
+        public static void Move(DIRECTIONS direction)
+        {
+            string wave;
+            switch (direction)
+            {
+                case DIRECTIONS.UP:
+                    wave = "Adelante.wav";
+                    break;
+                case DIRECTIONS.DOWN:
+                    wave = "Atras.wav";
+                    break;
+                case DIRECTIONS.RIGHT:
+                    wave = "Derecha.wav";
+                    break;
+                case DIRECTIONS.LEFT:
+                    wave = "Izquierda.wav";
+                    break;
+                default: throw new ArgumentOutOfRangeException(direction.ToString());
+            }
+            BroadCast(wave);
+        }
+        private static void BroadCast(string waveFile)
+        {
+            FileInfo wavInfo = new FileInfo($"{Environment.CurrentDirectory}/Samples/{waveFile}");
+            if (!wavInfo.Exists)
+            {
+                throw new FileNotFoundException(wavInfo.FullName);
+            }
+            ProcessStartInfo info = new ProcessStartInfo()
+            {
+                FileName = $"hackrf_transfer -f 27.145 -s 2 -l 14 -t \"{wavInfo.FullName}\"",
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true,
+            };
+            Debug.WriteLine($"MOVE - {waveFile}");
+            try
+            {
+                Process.Start(info).WaitForExitAsync().SafeFireAndForget();
+            }
+            catch { }
+        }
+    }
+}
